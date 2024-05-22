b054b7a86e386b781a7c604ee1407d4fa9fa7096
CodeX1991 <oghenewonaholy@gmail.com>
2024-05-22 22:37:21 +0000

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Caching system
+"""
+
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Basic caching system without limit
+    """
+    def __init__(self):
+        """
+        Initialize the cache
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key: str, item: any) -> None:
+        """
+        Add an item to the cache
+
+        Args:
+            key (str): the key under which the item should be stored
+            item (any): the item to be stored
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lst_recently_usd_key, _ = self.cache_data.popitem(last=True)
+                print("DISCARD: " + lst_recently_usd_key)
+
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+
+    def get(self, key: str) -> any:
+        """
+        Get an item by key from the cache
+
+        Args:
+            key (str): The key of the item to retrieve
+
+        Returns:
+            any: the item stored in the cache or None if the key doesnt exist
+        """
+        if key not in self.cache_data:
+            return None
+        else:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
+
+    def print_cache(self):
+        """
+        Print the contents of the cache in order
+        """
+        print("Current cache:")
+        for key, value in self.cache_data.items():
+            print(key + ": " + value)

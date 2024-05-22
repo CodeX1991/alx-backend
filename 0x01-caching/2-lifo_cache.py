630d5739a677091971ab43dcccb099289d6ffdd8
CodeX1991 <oghenewonaholy@gmail.com>
2024-05-22 17:09:22 +0000

    LIFO Caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Basic caching system without limit
+    """
+    def __init__(self):
+        """
+        Initialize the cache
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key: str, item: any) -> None:
+        """
+        Add an item to the cache
+
+        Args:
+            key (str): the key under which the item should be stored
+            item (any): the item to be stored
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                oldest_key = self.order.pop()
+                del self.cache_data[oldest_key]
+                print("DISCARD: " + oldest_key)
+
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key: str) -> any:
+        """
+        Get an item by key from the cache
+
+        Args:
+            key (str): The key of the item to retrieve
+
+        Returns:
+            any: the item stored in the cache or None if the key doesnt exist
+        """
+        return self.cache_data.get(key)
